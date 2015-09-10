cc1c4e5b3edb1db17863243daef5e8f4ef206cd9
eldevan <eldevanjr@iservport.com>
2015-09-10 15:53:10 -0300

    Repositories updated

--- src/main/java/org/helianto/finance/repository/AbstractFinanceRepository.java
+++ src/main/java/org/helianto/finance/repository/AbstractFinanceRepository.java
@@ -17,54 +17,14 @@ import org.springframework.data.jpa.repository.Query;
  *
  */
 public interface AbstractFinanceRepository extends JpaRepository<AbstractFinance, Serializable> {
-
-	/**
-	 * Find list by entity.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	List<AbstractFinance> findByEntity_Id(Integer entityId);
 	
 	/**
-	 * Find by privateEntity.
-	 * 
-	 * @param privateEntityId
+	 * Entities queries.
 	 */
-	List<AbstractFinance> findByPrivateEntity_Id(Integer privateEntityId);
 	
-	/**
-	 * Find list by entity and Resolution.
-	 * 
-	 * @param entityId
-	 * 
+	/*
+	 * SUM'S
 	 */
-	List<AbstractFinance> findByEntity_IdAndResolutionIn(Integer entityId, Set<Character> resolutions);
-	
-	/**
-	 * Find by privateEntity and Resolution.
-	 * 
-	 * @param privateEntityId
-	 */
-	List<AbstractFinance> findByPrivateEntity_IdAndResolutionIn(Integer privateEntityId, Set<Character> resolutions);
-	
-	/**
-	 * Sum faceValue by entityId.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	@Query("SELECT SUM(fin.faceValue) FROM AbstractFinance fin WHERE fin.entity.id = ?1 ")
-	BigDecimal sumFaceValueByEntityId(Integer entityId);
-	
-	/**
-	 * Sum faceValue by privateEntityId.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	@Query("SELECT SUM(fin.faceValue) FROM AbstractFinance fin WHERE fin.privateEntity.id = ?1 ")
-	BigDecimal sumFaceValueByPrivateEntityId(Integer privateEntityId);
 	
 	/**
 	 * Sum faceValue by entityId and Resolution.
@@ -77,18 +37,6 @@ public interface AbstractFinanceRepository extends JpaRepository<AbstractFinance
 		+ " WHERE fin.entity.id = ?1 "
 		+ " AND fin.resolution in (?2) ")
 	BigDecimal sumFaceValueByEntityIdAndResolutionIn(Integer entityId, Set<Character> resolutions);
-	
-	/**
-	 * Sum faceValue by privateEntityId and Resolution.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	@Query("SELECT SUM(fin.faceValue) "
-		+ " FROM AbstractFinance fin "
-		+ " WHERE fin.privateEntity.id = ?1 "
-		+ " AND fin.resolution in (?2) ")
-	BigDecimal sumFaceValueByPrivateEntityIdAndResolutionIn(Integer privateEntityId, Set<Character> resolutions);
 	
 	/**
 	 * Sum faceValue by entityId issueDate after startDate.
@@ -104,19 +52,6 @@ public interface AbstractFinanceRepository extends JpaRepository<AbstractFinance
 	BigDecimal sumFaceValueByEntityIdAndIssueDateAfter(Integer entityId, Date start, Set<Character> resolutions);
 	
 	/**
-	 * Sum faceValue by privateEntityId issueDate after startDate.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	@Query("SELECT SUM(fin.faceValue) "
-		+ " FROM AbstractFinance fin"
-		+ " WHERE fin.privateEntity.id = ?1 "
-		+ " AND fin.issueDate > ?2"
-		+ " AND fin.resolution in (?3)")
-	BigDecimal sumFaceValueByPrivateEntityIdAndIssueDateAfter(Integer privateEntityId, Date start, Set<Character> resolutions);
-	
-	/**
 	 * Sum faceValue by entityId nextCheckDate after startDate.
 	 * 
 	 * @param entityId
@@ -128,19 +63,19 @@ public interface AbstractFinanceRepository extends JpaRepository<AbstractFinance
 		+ " AND fin.nextCheckDate > ?2 "
 		+ " AND fin.resolution in (?3)")
 	BigDecimal sumFaceValueByEntityIdAndNextCheckDateAfter(Integer entityId, Date start, Set<Character> resolutions);
-	
+
 	/**
-	 * Sum faceValue by privateEntityId nextCheckDate after startDate.
+	 * Sum faceValue by entityId PaymentDate after startDate.
 	 * 
 	 * @param entityId
 	 * 
 	 */
 	@Query("SELECT SUM(fin.faceValue) "
-		+ " FROM AbstractFinance fin"
-		+ " WHERE fin.privateEntity.id = ?1 "
-		+ " AND fin.nextCheckDate > ?2 "
-		+ " AND fin.resolution in (?3)")
-	BigDecimal sumFaceValueByPrivateEntityIdAndNextCheckDateAfter(Integer privateEntityId, Date start, Set<Character> resolutions);
+		+ " FROM AbstractFinance fin "
+		+ " WHERE fin.entity.id = ?1 "
+		+ " AND fin.paymentDate > ?2 "
+		+ " AND fin.resolution in (?3) ")
+	BigDecimal sumFaceValueByEntityIdAndPaymentDateAfter(Integer entityId, Date start, Set<Character> resolutions);
 	
 	/**
 	 * Sum faceValue by entityId issueDate between dates.
@@ -156,19 +91,6 @@ public interface AbstractFinanceRepository extends JpaRepository<AbstractFinance
 	BigDecimal sumFaceValueByEntityIdAndIssueDateBetween(Integer entityId, Date start, Date end, Set<Character> resolutions);
 	
 	/**
-	 * Sum faceValue by privateEntityId issueDate between dates.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	@Query("SELECT SUM(fin.faceValue) "
-		+ "	FROM AbstractFinance fin "
-		+ " WHERE fin.privateEntity.id = ?1 "
-		+ " AND (fin.issueDate BETWEEN ?2 AND ?3) "
-		+ " AND fin.resolution in (?4)")
-	BigDecimal sumFaceValueByPrivateEntityIdAndIssueDateBetween(Integer privateEntityId, Date start, Date end, Set<Character> resolutions);
-	
-	/**
 	 * Sum faceValue by entityId nextCheckDate between dates.
 	 * 
 	 * @param entityId
@@ -182,17 +104,38 @@ public interface AbstractFinanceRepository extends JpaRepository<AbstractFinance
 	BigDecimal sumFaceValueByEntityIdAndNextCheckDateBetween(Integer entityId, Date start, Date end, Set<Character> resolutions);
 	
 	/**
-	 * Sum faceValue by privateEntityId nextCheckDate between dates.
+	 * Sum faceValue by entityId PaymentDate between dates.
 	 * 
 	 * @param entityId
 	 * 
 	 */
 	@Query("SELECT SUM(fin.faceValue) "
-		 + " FROM AbstractFinance fin "
-		 + " WHERE fin.privateEntity.id = ?1 "
-		 + " AND (fin.nextCheckDate BETWEEN ?2 AND ?3) "
-		 + " AND fin.resolution in (?4) ")
-	BigDecimal sumFaceValueByPrivateEntityIdAndNextCheckDateBetween(Integer privateEntityId, Date start, Date end, Set<Character> resolutions);
+		+ " FROM AbstractFinance fin "
+		+ " WHERE fin.entity.id = ?1 "
+		+ " AND (fin.paymentDate BETWEEN ?2 AND ?3) "
+		+ " AND fin.resolution in (?4)  ")
+	BigDecimal sumFaceValueByEntityIdAndPaymentDateBetween(Integer entityId, Date start, Date end, Set<Character> resolutions);
+	
+	/*
+	 * FIND'S
+	 */
+	
+	/**
+	 * Find list by entity.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	List<AbstractFinance> findByEntity_Id(Integer entityId);
+	
+	/**
+	 * Find list by entity and Resolution.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	List<AbstractFinance> findByEntity_IdAndResolutionIn(Integer entityId, Set<Character> resolutions);
+
 	
 	/**
 	 * Find by entity between issue date.
@@ -213,6 +156,176 @@ public interface AbstractFinanceRepository extends JpaRepository<AbstractFinance
 	List<AbstractFinance> findByEntity_IdAndResolutionInAndNextCheckDateBetween(Integer entityId, Set<Character> resolutions, Date start, Date end);
 	
 	/**
+	 * Find by private entity between PaymentDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<AbstractFinance> findByEntity_IdAndResolutionInAndPaymentDateBetween(Integer privateEntityId, Set<Character> resolutions, Date start, Date end);
+	
+	/**
+	 * Find by entity issue date after startDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<AbstractFinance> findByEntity_IdAndResolutionInAndIssueDateAfter(Integer entityId, Set<Character> resolutions, Date start);
+	
+	/**
+	 * Find by entity issue date after startDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<AbstractFinance> findByEntity_IdAndResolutionInAndNextCheckDateAfter(Integer entityId, Set<Character> resolutions, Date start);
+	
+	/**
+	 * Find by entity PaymentDate after startDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<AbstractFinance> findByEntity_IdAndResolutionInAndPaymentDateAfter(Integer entityId, Set<Character> resolutions, Date start);
+	
+	/**
+	 * Private Entities queries.
+	 */
+	
+	/*
+	 * SUM'S
+	 */
+	
+	/**
+	 * Sum faceValue by entityId.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) FROM AbstractFinance fin WHERE fin.entity.id = ?1 ")
+	BigDecimal sumFaceValueByEntityId(Integer entityId);
+	
+	/**
+	 * Sum faceValue by privateEntityId.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) FROM AbstractFinance fin WHERE fin.privateEntity.id = ?1 ")
+	BigDecimal sumFaceValueByPrivateEntityId(Integer privateEntityId);
+	
+	/**
+	 * Sum faceValue by privateEntityId and Resolution.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ " FROM AbstractFinance fin "
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND fin.resolution in (?2) ")
+	BigDecimal sumFaceValueByPrivateEntityIdAndResolutionIn(Integer privateEntityId, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId issueDate after startDate.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ " FROM AbstractFinance fin"
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND fin.issueDate > ?2"
+		+ " AND fin.resolution in (?3)")
+	BigDecimal sumFaceValueByPrivateEntityIdAndIssueDateAfter(Integer privateEntityId, Date start, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId nextCheckDate after startDate.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ " FROM AbstractFinance fin"
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND fin.nextCheckDate > ?2 "
+		+ " AND fin.resolution in (?3)")
+	BigDecimal sumFaceValueByPrivateEntityIdAndNextCheckDateAfter(Integer privateEntityId, Date start, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId PaymentDate after startDate.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ " FROM AbstractFinance fin"
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND fin.paymentDate > ?2"
+		+ " AND fin.resolution in (?3)")
+	BigDecimal sumFaceValueByPrivateEntityIdAndPaymentDateAfter(Integer privateEntityId, Date start, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId issueDate between dates.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ "	FROM AbstractFinance fin "
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND (fin.issueDate BETWEEN ?2 AND ?3) "
+		+ " AND fin.resolution in (?4)")
+	BigDecimal sumFaceValueByPrivateEntityIdAndIssueDateBetween(Integer privateEntityId, Date start, Date end, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId nextCheckDate between dates.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		 + " FROM AbstractFinance fin "
+		 + " WHERE fin.privateEntity.id = ?1 "
+		 + " AND (fin.nextCheckDate BETWEEN ?2 AND ?3) "
+		 + " AND fin.resolution in (?4) ")
+	BigDecimal sumFaceValueByPrivateEntityIdAndNextCheckDateBetween(Integer privateEntityId, Date start, Date end, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId PaymentDate between dates.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ "	FROM AbstractFinance fin "
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND (fin.paymentDate BETWEEN ?2 AND ?3) "
+		+ " AND fin.resolution in (?4)")
+	BigDecimal sumFaceValueByPrivateEntityIdAndPaymentDateBetween(Integer privateEntityId, Date start, Date end, Set<Character> resolutions);
+	
+	/*
+	 * FIND'S
+	 */
+	
+	/**
+	 * Find by privateEntity.
+	 * 
+	 * @param privateEntityId
+	 */
+	List<AbstractFinance> findByPrivateEntity_Id(Integer privateEntityId);
+	
+	/**
+	 * Find by privateEntity and Resolution.
+	 * 
+	 * @param privateEntityId
+	 */
+	List<AbstractFinance> findByPrivateEntity_IdAndResolutionIn(Integer privateEntityId, Set<Character> resolutions);
+	
+	/**
 	 * Find by private entity between issue date.
 	 * 
 	 * @param entityId
@@ -230,15 +343,15 @@ public interface AbstractFinanceRepository extends JpaRepository<AbstractFinance
 	 */
 	List<AbstractFinance> findByPrivateEntity_IdAndResolutionInAndNextCheckDateBetween(Integer privateEntityId, Set<Character> resolutions, Date start, Date end);
 	
+	
 	/**
-	 * Find by entity issue date after startDate.
+	 * Find by private entity between PaymentDate.
 	 * 
 	 * @param entityId
 	 * @param start
 	 * @param end
 	 */
-	List<AbstractFinance> findByEntity_IdAndResolutionInAndIssueDateAfter(Integer entityId, Set<Character> resolutions, Date start);
-	
+	List<AbstractFinance> findByPrivateEntity_IdAndResolutionInAndPaymentDateBetween(Integer privateEntityId, Set<Character> resolutions, Date start, Date end);
 	
 	/**
 	 * Find by private entity issue date after startDate.
@@ -248,5 +361,23 @@ public interface AbstractFinanceRepository extends JpaRepository<AbstractFinance
 	 * @param end
 	 */
 	List<AbstractFinance> findByPrivateEntity_IdAndResolutionInAndIssueDateAfter(Integer privateEntityId, Set<Character> resolutions, Date start);
+	
+	/**
+	 * Find by private entity nextcheckDate after startDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<AbstractFinance> findByPrivateEntity_IdAndResolutionInAndNextCheckDateAfter(Integer privateEntityId, Set<Character> resolutions, Date start);
+	
+	/**
+	 * Find by private entity PaymentDate after startDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<AbstractFinance> findByPrivateEntity_IdAndResolutionInAndPaymentDateAfter(Integer privateEntityId, Set<Character> resolutions, Date start);
 
 }
--- src/main/java/org/helianto/finance/repository/PayableRepository.java
+++ src/main/java/org/helianto/finance/repository/PayableRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.helianto.finance.domain.Payable;
+import org.helianto.finance.domain.Payable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -19,52 +20,12 @@ import org.springframework.data.jpa.repository.Query;
 public interface PayableRepository extends JpaRepository<Payable, Serializable> {
 
 	/**
-	 * Find list by entity.
-	 * 
-	 * @param entityId
-	 * 
+	 * Entities queries.
 	 */
-	List<Payable> findByEntity_Id(Integer entityId);
 	
-	/**
-	 * Find by privateEntity.
-	 * 
-	 * @param privateEntityId
+	/*
+	 * SUM'S
 	 */
-	List<Payable> findByPrivateEntity_Id(Integer privateEntityId);
-	
-	/**
-	 * Find list by entity and Resolution.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	List<Payable> findByEntity_IdAndResolutionIn(Integer entityId, Set<Character> resolutions);
-	
-	/**
-	 * Find by privateEntity and Resolution.
-	 * 
-	 * @param privateEntityId
-	 */
-	List<Payable> findByPrivateEntity_IdAndResolutionIn(Integer privateEntityId, Set<Character> resolutions);
-	
-	/**
-	 * Sum faceValue by entityId.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	@Query("SELECT SUM(fin.faceValue) FROM Payable fin WHERE fin.entity.id = ?1 ")
-	BigDecimal sumFaceValueByEntityId(Integer entityId);
-	
-	/**
-	 * Sum faceValue by privateEntityId.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	@Query("SELECT SUM(fin.faceValue) FROM Payable fin WHERE fin.privateEntity.id = ?1 ")
-	BigDecimal sumFaceValueByPrivateEntityId(Integer privateEntityId);
 	
 	/**
 	 * Sum faceValue by entityId and Resolution.
@@ -77,18 +38,6 @@ public interface PayableRepository extends JpaRepository<Payable, Serializable> 
 		+ " WHERE fin.entity.id = ?1 "
 		+ " AND fin.resolution in (?2) ")
 	BigDecimal sumFaceValueByEntityIdAndResolutionIn(Integer entityId, Set<Character> resolutions);
-	
-	/**
-	 * Sum faceValue by privateEntityId and Resolution.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	@Query("SELECT SUM(fin.faceValue) "
-		+ " FROM Payable fin "
-		+ " WHERE fin.privateEntity.id = ?1 "
-		+ " AND fin.resolution in (?2) ")
-	BigDecimal sumFaceValueByPrivateEntityIdAndResolutionIn(Integer privateEntityId, Set<Character> resolutions);
 	
 	/**
 	 * Sum faceValue by entityId issueDate after startDate.
@@ -104,19 +53,6 @@ public interface PayableRepository extends JpaRepository<Payable, Serializable> 
 	BigDecimal sumFaceValueByEntityIdAndIssueDateAfter(Integer entityId, Date start, Set<Character> resolutions);
 	
 	/**
-	 * Sum faceValue by privateEntityId issueDate after startDate.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	@Query("SELECT SUM(fin.faceValue) "
-		+ " FROM Payable fin"
-		+ " WHERE fin.privateEntity.id = ?1 "
-		+ " AND fin.issueDate > ?2"
-		+ " AND fin.resolution in (?3)")
-	BigDecimal sumFaceValueByPrivateEntityIdAndIssueDateAfter(Integer privateEntityId, Date start, Set<Character> resolutions);
-	
-	/**
 	 * Sum faceValue by entityId nextCheckDate after startDate.
 	 * 
 	 * @param entityId
@@ -128,19 +64,19 @@ public interface PayableRepository extends JpaRepository<Payable, Serializable> 
 		+ " AND fin.nextCheckDate > ?2 "
 		+ " AND fin.resolution in (?3)")
 	BigDecimal sumFaceValueByEntityIdAndNextCheckDateAfter(Integer entityId, Date start, Set<Character> resolutions);
-	
+
 	/**
-	 * Sum faceValue by privateEntityId nextCheckDate after startDate.
+	 * Sum faceValue by entityId PaymentDate after startDate.
 	 * 
 	 * @param entityId
 	 * 
 	 */
 	@Query("SELECT SUM(fin.faceValue) "
-		+ " FROM Payable fin"
-		+ " WHERE fin.privateEntity.id = ?1 "
-		+ " AND fin.nextCheckDate > ?2 "
-		+ " AND fin.resolution in (?3)")
-	BigDecimal sumFaceValueByPrivateEntityIdAndNextCheckDateAfter(Integer privateEntityId, Date start, Set<Character> resolutions);
+		+ " FROM Payable fin "
+		+ " WHERE fin.entity.id = ?1 "
+		+ " AND fin.paymentDate > ?2 "
+		+ " AND fin.resolution in (?3) ")
+	BigDecimal sumFaceValueByEntityIdAndPaymentDateAfter(Integer entityId, Date start, Set<Character> resolutions);
 	
 	/**
 	 * Sum faceValue by entityId issueDate between dates.
@@ -156,19 +92,6 @@ public interface PayableRepository extends JpaRepository<Payable, Serializable> 
 	BigDecimal sumFaceValueByEntityIdAndIssueDateBetween(Integer entityId, Date start, Date end, Set<Character> resolutions);
 	
 	/**
-	 * Sum faceValue by privateEntityId issueDate between dates.
-	 * 
-	 * @param entityId
-	 * 
-	 */
-	@Query("SELECT SUM(fin.faceValue) "
-		+ "	FROM Payable fin "
-		+ " WHERE fin.privateEntity.id = ?1 "
-		+ " AND (fin.issueDate BETWEEN ?2 AND ?3) "
-		+ " AND fin.resolution in (?4)")
-	BigDecimal sumFaceValueByPrivateEntityIdAndIssueDateBetween(Integer privateEntityId, Date start, Date end, Set<Character> resolutions);
-	
-	/**
 	 * Sum faceValue by entityId nextCheckDate between dates.
 	 * 
 	 * @param entityId
@@ -182,17 +105,38 @@ public interface PayableRepository extends JpaRepository<Payable, Serializable> 
 	BigDecimal sumFaceValueByEntityIdAndNextCheckDateBetween(Integer entityId, Date start, Date end, Set<Character> resolutions);
 	
 	/**
-	 * Sum faceValue by privateEntityId nextCheckDate between dates.
+	 * Sum faceValue by entityId PaymentDate between dates.
 	 * 
 	 * @param entityId
 	 * 
 	 */
 	@Query("SELECT SUM(fin.faceValue) "
-		 + " FROM Payable fin "
-		 + " WHERE fin.privateEntity.id = ?1 "
-		 + " AND (fin.nextCheckDate BETWEEN ?2 AND ?3) "
-		 + " AND fin.resolution in (?4) ")
-	BigDecimal sumFaceValueByPrivateEntityIdAndNextCheckDateBetween(Integer privateEntityId, Date start, Date end, Set<Character> resolutions);
+		+ " FROM Payable fin "
+		+ " WHERE fin.entity.id = ?1 "
+		+ " AND (fin.paymentDate BETWEEN ?2 AND ?3) "
+		+ " AND fin.resolution in (?4)  ")
+	BigDecimal sumFaceValueByEntityIdAndPaymentDateBetween(Integer entityId, Date start, Date end, Set<Character> resolutions);
+	
+	/*
+	 * FIND'S
+	 */
+	
+	/**
+	 * Find list by entity.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	List<Payable> findByEntity_Id(Integer entityId);
+	
+	/**
+	 * Find list by entity and Resolution.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	List<Payable> findByEntity_IdAndResolutionIn(Integer entityId, Set<Character> resolutions);
+
 	
 	/**
 	 * Find by entity between issue date.
@@ -213,6 +157,176 @@ public interface PayableRepository extends JpaRepository<Payable, Serializable> 
 	List<Payable> findByEntity_IdAndResolutionInAndNextCheckDateBetween(Integer entityId, Set<Character> resolutions, Date start, Date end);
 	
 	/**
+	 * Find by private entity between PaymentDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<Payable> findByEntity_IdAndResolutionInAndPaymentDateBetween(Integer privateEntityId, Set<Character> resolutions, Date start, Date end);
+	
+	/**
+	 * Find by entity issue date after startDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<Payable> findByEntity_IdAndResolutionInAndIssueDateAfter(Integer entityId, Set<Character> resolutions, Date start);
+	
+	/**
+	 * Find by entity issue date after startDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<Payable> findByEntity_IdAndResolutionInAndNextCheckDateAfter(Integer entityId, Set<Character> resolutions, Date start);
+	
+	/**
+	 * Find by entity PaymentDate after startDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<Payable> findByEntity_IdAndResolutionInAndPaymentDateAfter(Integer entityId, Set<Character> resolutions, Date start);
+	
+	/**
+	 * Private Entities queries.
+	 */
+	
+	/*
+	 * SUM'S
+	 */
+	
+	/**
+	 * Sum faceValue by entityId.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) FROM Payable fin WHERE fin.entity.id = ?1 ")
+	BigDecimal sumFaceValueByEntityId(Integer entityId);
+	
+	/**
+	 * Sum faceValue by privateEntityId.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) FROM Payable fin WHERE fin.privateEntity.id = ?1 ")
+	BigDecimal sumFaceValueByPrivateEntityId(Integer privateEntityId);
+	
+	/**
+	 * Sum faceValue by privateEntityId and Resolution.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ " FROM Payable fin "
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND fin.resolution in (?2) ")
+	BigDecimal sumFaceValueByPrivateEntityIdAndResolutionIn(Integer privateEntityId, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId issueDate after startDate.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ " FROM Payable fin"
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND fin.issueDate > ?2"
+		+ " AND fin.resolution in (?3)")
+	BigDecimal sumFaceValueByPrivateEntityIdAndIssueDateAfter(Integer privateEntityId, Date start, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId nextCheckDate after startDate.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ " FROM Payable fin"
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND fin.nextCheckDate > ?2 "
+		+ " AND fin.resolution in (?3)")
+	BigDecimal sumFaceValueByPrivateEntityIdAndNextCheckDateAfter(Integer privateEntityId, Date start, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId PaymentDate after startDate.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ " FROM Payable fin"
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND fin.paymentDate > ?2"
+		+ " AND fin.resolution in (?3)")
+	BigDecimal sumFaceValueByPrivateEntityIdAndPaymentDateAfter(Integer privateEntityId, Date start, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId issueDate between dates.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ "	FROM Payable fin "
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND (fin.issueDate BETWEEN ?2 AND ?3) "
+		+ " AND fin.resolution in (?4)")
+	BigDecimal sumFaceValueByPrivateEntityIdAndIssueDateBetween(Integer privateEntityId, Date start, Date end, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId nextCheckDate between dates.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		 + " FROM Payable fin "
+		 + " WHERE fin.privateEntity.id = ?1 "
+		 + " AND (fin.nextCheckDate BETWEEN ?2 AND ?3) "
+		 + " AND fin.resolution in (?4) ")
+	BigDecimal sumFaceValueByPrivateEntityIdAndNextCheckDateBetween(Integer privateEntityId, Date start, Date end, Set<Character> resolutions);
+	
+	/**
+	 * Sum faceValue by privateEntityId PaymentDate between dates.
+	 * 
+	 * @param entityId
+	 * 
+	 */
+	@Query("SELECT SUM(fin.faceValue) "
+		+ "	FROM Payable fin "
+		+ " WHERE fin.privateEntity.id = ?1 "
+		+ " AND (fin.paymentDate BETWEEN ?2 AND ?3) "
+		+ " AND fin.resolution in (?4)")
+	BigDecimal sumFaceValueByPrivateEntityIdAndPaymentDateBetween(Integer privateEntityId, Date start, Date end, Set<Character> resolutions);
+	
+	/*
+	 * FIND'S
+	 */
+	
+	/**
+	 * Find by privateEntity.
+	 * 
+	 * @param privateEntityId
+	 */
+	List<Payable> findByPrivateEntity_Id(Integer privateEntityId);
+	
+	/**
+	 * Find by privateEntity and Resolution.
+	 * 
+	 * @param privateEntityId
+	 */
+	List<Payable> findByPrivateEntity_IdAndResolutionIn(Integer privateEntityId, Set<Character> resolutions);
+	
+	/**
 	 * Find by private entity between issue date.
 	 * 
 	 * @param entityId
@@ -230,15 +344,15 @@ public interface PayableRepository extends JpaRepository<Payable, Serializable> 
 	 */
 	List<Payable> findByPrivateEntity_IdAndResolutionInAndNextCheckDateBetween(Integer privateEntityId, Set<Character> resolutions, Date start, Date end);
 	
+	
 	/**
-	 * Find by entity issue date after startDate.
+	 * Find by private entity between PaymentDate.
 	 * 
 	 * @param entityId
 	 * @param start
 	 * @param end
 	 */
-	List<Payable> findByEntity_IdAndResolutionInAndIssueDateAfter(Integer entityId, Set<Character> resolutions, Date start);
-	
+	List<Payable> findByPrivateEntity_IdAndResolutionInAndPaymentDateBetween(Integer privateEntityId, Set<Character> resolutions, Date start, Date end);
 	
 	/**
 	 * Find by private entity issue date after startDate.
@@ -248,6 +362,24 @@ public interface PayableRepository extends JpaRepository<Payable, Serializable> 
 	 * @param end
 	 */
 	List<Payable> findByPrivateEntity_IdAndResolutionInAndIssueDateAfter(Integer privateEntityId, Set<Character> resolutions, Date start);
+	
+	/**
+	 * Find by private entity nextcheckDate after startDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<Payable> findByPrivateEntity_IdAndResolutionInAndNextCheckDateAfter(Integer privateEntityId, Set<Character> resolutions, Date start);
+	
+	/**
+	 * Find by private entity PaymentDate after startDate.
+	 * 
+	 * @param entityId
+	 * @param start
+	 * @param end
+	 */
+	List<Payable> findByPrivateEntity_IdAndResolutionInAndPaymentDateAfter(Integer privateEntityId, Set<Character> resolutions, Date start);
 
 	
 }
